62f110be48e2b392e6d876ea609cfbb971ef75e0
takuyaw-w <takuya.jamtb@gmail.com>
2022-07-28 21:46:24 +0900

    wip

--- database/seeders/TweetsSeeder.php
+++ database/seeders/TweetsSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\Tweet;
 
 class TweetsSeeder extends Seeder
 {
@@ -16,11 +17,6 @@ class TweetsSeeder extends Seeder
      */
     public function run()
     {
-        //
-        DB::table('tweets')->insert([
-            'content' => Str::random(100),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        Tweet::factory()->count(10)->create();
     }
 }
