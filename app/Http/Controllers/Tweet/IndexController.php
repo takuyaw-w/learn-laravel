44e4f63227bb299b1caf4ea7914773e9eb4c87a9
takuyaw-w <takuya.jamtb@gmail.com>
2022-07-28 21:39:49 +0900

    update: add single action controller

---
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+        return view('tweet.index', ['name' => 'laravel']);
+    }
+}
